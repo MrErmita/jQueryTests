7c7fff843180bdfe58f363115a49d03936b963c1
lwsioo <alessio.bragetti@gmail.com>
2024-10-18 09:15:38 +0200

    commit
    apdl

--- jquery.js
+++ jquery.js
@@ -1,66 +1,51 @@
+function optim(id, regex, error) {
+  if (regex.test(id.value)) {
+    $(id).css({
+      "border": "2px solid #4B6043", "background-color": "#95BB72"});
+    $(error).css("display", "none");
+    console.log("ok");
+  } else {
+    $(id).css({
+      "border": "2px solid #A52A2A", "background-color": "#FFC0C0"});
+    $(error).css("display", "block");
+    console.log("no");
+  }
+}
+
 $(document).ready(function () {
-    // Espressioni regolari per la validazione
-    const LETTERS_REGEX = /^[a-zA-Z]+$/;
-    const PHONE_REGEX = /^(\+\d{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
-    const CF_REGEX = /^[A-Z]{6}[0-9]{2}[A-Z][0-9]{2}[A-Z][0-9]{3}[A-Z]$/i;
-    const STREET_REGEX = /^[A-Za-z0-9 ]+$/;
-  
-    // Funzione per validare i campi di input
-    function validateField(input, regex, errorId) {
-      const value = input.val();
-      if (regex.test(value)) {
-        $(errorId).hide();
-        input.css("border", "2px solid #4B6043");
-      } else {
-        $(errorId).show();
-        input.css("border", "2px solid #A52A2A");
-      }
-    }
-  
-    // Validazione dei campi al momento dell'input
-    $("#nameIn").on("input", function () {
-      validateField($(this), LETTERS_REGEX, "#nameError");
-    });
-  
-    $("#surnameIn").on("input", function () {
-      validateField($(this), LETTERS_REGEX, "#surnameError");
-    });
-  
-    $("#phoneIn").on("input", function () {
-      validateField($(this), PHONE_REGEX, "#phoneError");
-    });
-  
-    $("#streetIn").on("input", function () {
-      validateField($(this), STREET_REGEX, "#streetError");
-    });
-  
-    $("#cFiscIn").on("input", function () {
-      validateField($(this), CF_REGEX, "#cFiscError");
-    });
-  
-    $("#ageIn").on("input", function () {
-      const age = $(this).val();
-      const parentInfo = $("#parentInfo");
-  
-      if (age >= 0 && age <= 99) {
-        $("#ageError").hide();
-        if (age < 18) {
-          parentInfo.show();
-          $(".pInfos").attr("required", "required");
-        } else {
-          parentInfo.hide();
-          $(".pInfos").removeAttr("required");
-        }
-      } else {
-        $("#ageError").show();
-        parentInfo.hide();
-      }
-    });
-  
-    // Funzione di validazione finale prima dell'invio
-    $("#submitBtn").on("click", function () {
-      // Qui puoi aggiungere ulteriori controlli prima dell'invio del modulo
-      alert("Form submitted!");
-    });
+  const LETTERS_REGEX = /^[a-zA-Z]+$/;
+  const PHONE_REGEX = /^(\+\d{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
+  const STREET_REGEX = /^[A-Za-z0-9 , / ]+$/;
+  const CF_REGEX = /^[A-Z]{6}[0-9]{2}[A-Z][0-9]{2}[A-Z][0-9]{3}[A-Z]$/i;
+
+  //controllo generale per tutti i campi
+  $("#nameIn").on("input", function () {
+    optim(this, LETTERS_REGEX, "#nameError");
+    console.log(this.value);
   });
-  
+  $("#surnameIn").on("input", function () {
+    optim(this, LETTERS_REGEX, "#surnameError");
+    console.log(this.value);
+  });
+  $("#phoneIn").on("input", function () {
+    optim(this, PHONE_REGEX, "#phoneError");
+    console.log(this.value);
+  });
+  $("#streetIn").on("input", function () {
+    optim(this, STREET_REGEX, "#streetError");
+    console.log(this.value);
+  });
+  $("#cFiscIn").on("input", function () {
+    optim(this, CF_REGEX, "#cFiscError");
+    console.log(this.value);
+  });
+
+  //controllo specifico per l'età
+  $("#ageIn").on("input", function () {
+    let age = parseInt(this.value);
+    let pInfoElements = $(".pInfos");
+    if(!isNaN(age) && age > 0 && age < 100) {
+      
+    }
+  });
+});
